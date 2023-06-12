99a8b30e1f8b735132d324bd37cf02a64cb05701
Mikhail T <Mikola301@mail.ru>
2023-06-12 23:44:12 +0700

    Добавлена addTask2.1 и также с генератором task1

---
+++ addTask2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 9999]
+// и показывает наибольшую цифру числа.
+//  7812 -> 8 
+//  1213-> 3 
+//  845 -> 8
+
+int rand = new Random().Next(10, 9999);
+
+int Max(int rand)
+{
+    int i = 0;
+    while(rand > 0)
+    {
+        int rem10 = rand % 10; 
+        if (rem10 > i)
+        {
+            i = rem10;
+        }
+        rand /= 10; 
+    }
+    return i;
+}
+
+System.Console.WriteLine($"Число: {rand}. Максимальная цифра: {Max(rand)}");
--- task1/Program.cs
+++ task1/Program.cs
@@ -8,20 +8,3 @@ int ranNum = new Random().Next(100, 1000);
 
 System.Console.WriteLine(ranNum);
 System.Console.WriteLine(ranNum / 10 % 10);
-
-// System.Console.WriteLine(Max(ranNum));
-
-
-// int Max(int ranNum)
-// {
-//     int num1 = ranNum / 10;
-//     int num2 = ranNum % 10; 
-//     if (num1 > num2)
-//     {
-//         return num1;
-//     }
-//     else
-//     {
-//         return num2;
-//     }
-// }
