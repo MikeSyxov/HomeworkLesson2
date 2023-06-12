1441a9dcc83620caf6cabcb28cffc8fc1aab8ab0
Mikhail T <Mikola301@mail.ru>
2023-06-12 23:16:36 +0700

    Добалены условия дополнительныйх задач readme

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+// примеры:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ranNum = new Random().Next(100, 1000);
+
+System.Console.WriteLine(ranNum);
+System.Console.WriteLine(ranNum / 10 % 10);
+
+// System.Console.WriteLine(Max(ranNum));
+
+
+// int Max(int ranNum)
+// {
+//     int num1 = ranNum / 10;
+//     int num2 = ranNum % 10; 
+//     if (num1 > num2)
+//     {
+//         return num1;
+//     }
+//     else
+//     {
+//         return num2;
+//     }
+// }
