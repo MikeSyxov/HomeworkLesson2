a3d9e4139a81eeb7fb85065f4e5f320fcb93e50c
Mikhail T <Mikola301@mail.ru>
2023-06-14 23:39:47 +0700

    add code with task3.1

--- addTask3.1/Program.cs
+++ addTask3.1/Program.cs
@@ -2,3 +2,28 @@
 // и выводить сумму и среднее арифметическое этих чисел.
 // 2, 5, 6, 8, 1 -> сумма 22, среднее 4,4 
 
+int ReadInt(string message)
+{
+    Console.Write(message);                                          // Вывод приглашения
+    int number = System.Convert.ToInt32(Console.ReadLine());           // Чтение строки и преобразование в число
+    return number;                                                     // Возврат целого числа
+}
+
+int Sum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int[] arr = new int[5];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = ReadInt($"Введите {i + 1} число: ");
+}
+
+Console.WriteLine($"Сумма чисел равна {Sum(arr)},среднее значение чисел: {(double)Sum(arr) / arr.Length}");
